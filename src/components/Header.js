ea21579ad187371f9728dd51ab3167fd710bb8f8
Artem Lavrinovich <art.lavrinovich@outlook.com>
2022-05-03 17:33:43 +0300

    First commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Progress from './Progress';
+
+const Header = () => (
+    <div>
+        <h1>Best jobs for digital marketing professionals</h1>
+        <Progress />
+    </div>
+);
+
+export default Header;
