0767d8cf77bf84d76f1243bca035307a4fc40c94
Artem Lavrinovich <art.lavrinovich@outlook.com>
2022-05-03 20:53:16 +0300

    Original version

--- src/components/SecondStep.js
+++ src/components/SecondStep.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import { Form, Button } from 'react-bootstrap';
+import { motion } from 'framer-motion';
 
 const SecondStep = (props) => {
-  const { register, handleSubmit, errors } = useForm();
+  const { user } = props;
+  const { register, handleSubmit, errors } = useForm({
+    defaultValues: {
+      user_email: user.user_email,
+      user_password: user.user_password
+    }
+  });
 
   const onSubmit = (data) => {
-    console.log(data);
+    props.updateUser(data);
+    props.history.push('/third');
   };
 
   return (
     <Form className="input-form" onSubmit={handleSubmit(onSubmit)}>
-      <div className="col-md-6 offset-md-3">
+      <motion.div
+        className="col-md-6 offset-md-3"
+        initial={{ x: '-100vw' }}
+        animate={{ x: 0 }}
+        transition={{ stiffness: 150 }}
+      >
         <Form.Group controlId="first_name">
           <Form.Label>Email</Form.Label>
           <Form.Control
@@ -57,7 +70,7 @@ const SecondStep = (props) => {
         <Button variant="primary" type="submit">
           Next
         </Button>
-      </div>
+      </motion.div>
     </Form>
   );
 };
--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,20 +1,57 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React, { useState } from 'react';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import FirstStep from '../components/FirstStep';
-import SecondStep from '../components/SecondStep';
 import Header from '../components/Header';
+import Login from '../components/Login';
+import SecondStep from '../components/SecondStep';
+import ThirdStep from '../components/ThirdStep';
 
-const AppRouter = () => (
+const AppRouter = () => {
+  const [user, setUser] = useState({});
+
+  const updateUser = (data) => {
+    setUser((prevUser) => ({ ...prevUser, ...data }));
+  };
+
+  const resetUser = () => {
+    setUser({});
+  };
+
+  return (
     <BrowserRouter>
-        <div className="container">
-            <Header />
-            <Switch>
-                <Route component={FirstStep} path="/" exact={true} />
-                <Route component={SecondStep} path="/second" />
-                <Route component={ThirdStep} path="/third" />
-            </Switch>
-        </div>
+      <div className="container">
+        <Header />
+        <Switch>
+          <Route
+            render={(props) => (
+              <FirstStep {...props} user={user} updateUser={updateUser} />
+            )}
+            path="/"
+            exact={true}
+          />
+          <Route
+            render={(props) => (
+              <SecondStep {...props} user={user} updateUser={updateUser} />
+            )}
+            path="/second"
+          />
+          <Route
+            render={(props) => (
+              <ThirdStep
+                {...props}
+                user={user}
+                updateUser={updateUser}
+                resetUser={resetUser}
+              />
+            )}
+            path="/third"
+          />
+          <Route component={Login} path="/login" />
+          <Route render={() => <Redirect to="/" />} />
+        </Switch>
+      </div>
     </BrowserRouter>
-);
+  );
+};
 
 export default AppRouter;
